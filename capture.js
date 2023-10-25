f10a782a8fbeb677daeaf0691bc5245c0146b74f
Dick Chan <dickhfchan@gmail.com>
2023-10-25 16:36:26 +0800

    change from 1080 to 1440

--- capture.js
+++ capture.js
@@ -433,8 +433,10 @@ function setShadowCast() {
   height_value = 1080;
 
   if (modeSelected === "Favor Resolution") {
-    width_value = 1920;
-    height_value = 1080;
+    // width_value = 1920;
+    // height_value = 1080;
+    width_value = 2560; // Set the 2K
+    height_value = 1440;
   } else {
     width_value = 1280;
     height_value = 720;
