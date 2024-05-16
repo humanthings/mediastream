8fde5177081627ff486be0f10fc880f7aec39915
Alex Smith <smithavt14@gmail.com>
2024-05-16 13:30:23 +0800

    Add necessary components for PWA

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "shadowcast-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/capture.js",
+  "/manifest.json",
+  "/assets/icons/icon-192x192.png",
+  "/assets/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
